ba00af8dd31def436ae490988e89efd7850e052e
Arun-913 <arunchau986@gmail.com>
2024-07-20 01:05:32 +0530

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['js-cookie'],
+    },
+  },
 })
