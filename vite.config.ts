5a0c1b242883883c3e0c48a87185cf1af0d99921
Arun-913 <arunchau986@gmail.com>
2024-07-19 23:31:31 +0530

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['js-cookie'], // Add 'js-cookie' to the external list
+    },
+  },
 })
